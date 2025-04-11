3b7b5627142258dd12fbffe93c6baeb5654f85c2
GetPhoneHub.com <807280597@qq.com>
2025-04-11 23:43:00 +0800

    Add files via upload

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -15,3 +15,12 @@ firebase.initializeApp(firebaseConfig);
 // 获取Auth和Database实例
 const auth = firebase.auth();
 const database = firebase.database();
+
+// 默认进行匿名登录以获取数据库访问权限
+auth.signInAnonymously()
+  .then(() => {
+    console.log('匿名登录成功，已获取数据库访问权限');
+  })
+  .catch((error) => {
+    console.error('匿名登录失败:', error);
+  });
