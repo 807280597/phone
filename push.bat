d1ae4d59a8b4353d58e01f04e9f8f6d881e04427
GetPhoneHub.com <807280597@qq.com>
2025-04-14 11:36:25 +0800

    更新手机网站: 周一 2025/04/14 11:36:25.62

--- push.bat
+++ push.bat
@@ -8,6 +8,9 @@ echo 正在取消代理设置...
 git config --global --unset http.proxy
 git config --global --unset https.proxy
 
+echo 正在同步远程代码...
+git pull origin main
+
 echo 正在推送手机网站代码到GitHub...
 git add .
 git commit -m "更新手机网站: %date% %time%"
